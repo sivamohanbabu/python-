1e57a28a1cf61638dbb4492c923528a074b9d07b
sivamohanbabu <sivamohanbabu@gmail.com>
2025-11-26 15:11:20 +0530

    added hello.py

---
+++ hello.py
@@ -0,0 +1,49 @@
+# print("hello world")
+# #comments in python
+# '''
+# print(hello) 
+# '''
+# #variables 
+# a = 12
+# b = 78
+# c = 4
+# a  = b
+# b = c
+# c = a
+# print(a)
+# print(b)
+# print(c)
+
+# name = "abc"
+# print(name)
+# a = int(input("enter the value"))
+# b = float(input("enter the float value"))
+
+# print(b)
+# print(type(b))
+
+# is_value = True
+# print(type(is_value))
+
+#primitive - number - int ,float, double,str,boolen,
+
+# a = 678689.80980
+# print(type(a))
+#non primitive
+name = ("raj","john","ram",78,34) #immtable
+ages = [56,78,23,90]#mutable
+print(type(name))
+print(type(ages))
+print(ages)
+
+books = {
+    'bookname' :'maxin',
+    'published':'1997'
+
+}
+print(type(books))
+
+dep = {"csc","csd","ece"}
+print(type(dep))
+
+
